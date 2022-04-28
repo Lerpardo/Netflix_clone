5f266a9e9605c520d95bd26fbf01ab4d50d56eb2
Lerpardo <ls6182315@gmail.com>
2022-04-28 15:52:44 -0300

    finalização

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
+function butao1(){
+    window.location.replace("https://www.youtube.com/watch?v=HJv4LQxbVEA&t=19s")
+}
+function butao2() {
+    window.location.replace("https://www.themoviedb.org/movie/414906-the-batman?language=pt-BR")
+}
